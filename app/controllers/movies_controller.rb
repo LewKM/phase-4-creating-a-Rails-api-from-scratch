fd5de1fbd6471607137fe9efdf45cd0862c2abb1
LewKM <lewiskathembe51@gmail.com>
2024-05-14 15:13:13 +0300

    Adda comment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
 
+        # A method that retrieves all movies and renders them in JSON format.
     def index
         movies = Movie.all
         render json: movies
